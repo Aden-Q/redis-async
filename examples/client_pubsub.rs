bec57ddf82951223199924f84b09c818c30be017
Aden-Q <qzc0512@gmail.com>
2025-03-31 23:09:57 -0500

    tests: add integration tests

--- examples/client_pubsub.rs
+++ examples/client_pubsub.rs
@@ -36,13 +36,42 @@
 //     Ok(())
 // }
 
+use redis_async::{Client, Result};
+use std::str;
+
 #[tokio::main]
-async fn main() -> mini_redis::Result<()> {
-    // tokio::spawn(async { publish().await });
+async fn main() -> Result<()> {
+    let mut client = Client::connect("127.0.0.1:6379").await?;
+    let response: Option<Vec<u8>> = client.set("mykey", "myvalue".as_bytes()).await?;
 
-    // subscribe().await?;
+    if let Some(value) = response {
+        if let Ok(string) = str::from_utf8(&value) {
+            println!("{}", string);
+        } else {
+            println!("{:?}", value);
+        }
+    } else {
+        println!("(nil)");
+    }
 
-    println!("Done");
+    let response = client.get("mykey").await?;
+    if let Some(value) = response {
+        if let Ok(string) = str::from_utf8(&value) {
+            println!("\"{}\"", string);
+        } else {
+            println!("{:?}", value);
+        }
+    } else {
+        println!("(nil)");
+    }
+
+    let resp = client.del(vec!["mykey"]).await?;
+
+    println!("DEL command response: {}", resp);
+
+    let resp = client.exists(vec!["mykey"]).await?;
+
+    println!("EXISTS command response: {}", resp);
 
     Ok(())
 }
